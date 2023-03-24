83cf7bd34b9b709694acfa2f9f434729dd7c54d3
MAKARnikanov <maka.nik.00@mail.ru>
2023-03-24 14:36:25 +0300

    create class Youtube

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("начало")
+import os
+import json
+
+from googleapiclient.discovery import build
+
+class Youtube:
+
+    def __init__(self, channel_id):
+        self.channel_id = channel_id
+
+        api_key: str = os.environ.get('API_KEY')
+        youtube = build('youtube', 'v3', developerKey = api_key)
+        self.channel = youtube.channels().list(id = channel_id, part = 'snippet,statistics').execute()
