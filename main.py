48d7c8783ff81a58c8c45693fa51d373ebc749be
MAKARnikanov <maka.nik.00@mail.ru>
2023-03-24 15:42:07 +0300

    Метод print_info

--- main.py
+++ main.py
@@ -3,11 +3,19 @@ import json
 
 from googleapiclient.discovery import build
 
+from dotenv import load_dotenv
+
 class Youtube:
 
     def __init__(self, channel_id):
         self.channel_id = channel_id
+        load_dotenv()
+        api_key: str = os.getenv('API_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        self.channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
 
-        api_key: str = os.environ.get('API_KEY')
-        youtube = build('youtube', 'v3', developerKey = api_key)
-        self.channel = youtube.channels().list(id = channel_id, part = 'snippet,statistics').execute()
+    def print_info(self):
+        print(json.dumps(self.channel, indent=2, ensure_ascii=False))
+
+vdud = Youtube('UCMCgOm8GZkHp8zJ6l7_hIuA')
+vdud.print_info()
